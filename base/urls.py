ca535227696e53daf156c4d820d6f28d4d30949f
Szymon Debski <szymonsitno@gmail.com>
2023-04-04 22:28:53 +0200

    cru(d) implemented

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.TaskList.as_view(), name='tasks')
+    path('', views.TaskList.as_view(), name='tasks'),
+    path('tasks/<str:pk>/', views.TaskDetail.as_view(), name='task'),
+    path('task-create/', views.TaskCreate.as_view(), name='task-create'),
+    path('task-update/<str:pk>', views.TaskUpdate.as_view(), name='task-update'),
 ]
